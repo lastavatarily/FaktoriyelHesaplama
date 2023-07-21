e7ea209d495ef8177a239d34edcb86c337f73040
ilayda kuruhan <ilaydakuruhan@gmail.com>
2023-07-21 23:30:06 +0300

    Add project files.

---
+++ Degiskennedir/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Degiskennedir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FAKTORİYEL");
+            Console.WriteLine("Faktoriyelini Almak İstediğiniz Sayıyı Giriniz:");
+            int sayı = Convert.ToInt32(Console.ReadLine());
+            int faktoriyel = 1;
+
+                while (sayı>1)
+            {   
+                faktoriyel *= sayı;
+
+                sayı--;
+
+            }
+            Console.WriteLine("Cevap:{0} Faktöriyeldir.", faktoriyel);
+            Console.ReadKey();
+        }  
+      
+    }
+
+}
+
